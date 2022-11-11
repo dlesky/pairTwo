bae4f01de1cbea72f94b5425aea13a6297e607cb
Dan Lesky <dan.lesky@gmail.com>
2022-11-11 15:07:59 -0700

    Add changes

--- friends.js
+++ friends.js
@@ -1,8 +1,7 @@
 var bestFriend = "May"
-if (bestFriend ==="Maya") {
-    console.log("Hi Best Friend!")
-} else {
-    console.log("Hi Stranger!")
-}
+var goodFriend = "Be"
+
+if (bestFriend==="Maya") {console.log("Hi Best Friend!")} else if(goodFriend==="Ben") {console.log("Hi good friend!")} else {console.log("Hi Stranger!")}
+
 
 
